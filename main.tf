f38998ed9251de60394b8395c676cc6d3d950020
arpydays <arpydays@daftdays.com>
2022-11-28 15:23:22 +1300

    adds vpc-id to instance

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  vpc_id  = module.vpc.vpc_id
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
