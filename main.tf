8776aa17e4c234963dddbad80c2b9aae6fa990ca
arpydays <arpydays@daftdays.com>
2022-11-28 16:02:48 +1300

    renames blog_vpc to vpc as it broke

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -34,7 +34,7 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami                     = data.aws_ami.app_ami.id
   instance_type           = var.instance_type
-  subnet_id               = module.blog_vpc.public_subnets[0]
+  subnet_id               = module.vpc.public_subnets[0]
   vpc_security_group_ids  = [module.blog_sg.security_group_id]
 
   tags = {
@@ -50,8 +50,8 @@ module "blog_alb" {
 
   load_balancer_type = "application"
 
-  vpc_id              = module.blog_vpc.vpc_id
-  subnets             = module.blog_vpc.public_subnets
+  vpc_id              = module.vpc.vpc_id
+  subnets             = module.vpc.public_subnets
   security_groups     = module.blog_sg.security_group_id
 
   target_groups = [
@@ -87,7 +87,7 @@ module "blog_sg" {
   version = "4.16.2"
   name    = "blog_new"
 
-  vpc_id  = module.blog_vpc.vpc_id
+  vpc_id  = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
